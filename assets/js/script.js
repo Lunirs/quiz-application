de31c23a6959837e946d58ccc689cadeb9e9fb95
Lunirs <dhong0925@gmail.com>
2022-05-17 20:20:00 -0400

    added Pseudo code

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// Start button that runs the quiz
+// Need start quiz function
+// When button is pressed, timer starts, and quiz questions are displayed
+
+// Question is answered (right or wrong)
+// render question function
+// displays question based on index
+// creates 4 answer choices all in button format
+// basically has an event.target to determine that the correct answer choice was clicked on
+//once clicked it checks our answer
+
+// function that checks our answer
+// if wrong then time penalty and move on to next question
+// displays incorrect
+// if correct then display correct then move onto next question.
+
+// countdown function
+// time goes down at an interval of 1 second
+// if time goes to 0 it ends the quiz
+// end the game
+// if user answers all questions and there are no more questions to be answered
+// we clear the interval for time left and end the game
+
+// function to end the game
+// displays the score page
+//asks user for name input
+//once submit button is pressed, then data is stored on local storage
+
+// data store function
+// if there isn't an input then return
+// else take the input and store on local storage
+
+//high score page
+// gets scores stored on local storage and displays them
+// clear high score button
+// function to delete local storage data
